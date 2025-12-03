7b43feeeb6df1dd7a9e3a9544339d0b05021e157
Kris2x <kwesek@gmail.com>
2025-12-03 22:45:13 +0100

    Add Product management features with controller, service, and views

--- src/Catalog/Controller/ProductController.php
+++ src/Catalog/Controller/ProductController.php
@@ -2,11 +2,41 @@
 
 namespace App\Catalog\Controller;
 
+use App\Catalog\Service\ProductService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/catalog/product', name: 'catalog_product_')]
 class ProductController extends AbstractController
 {
+  public function __construct(
+    private ProductService $productService,
+  )
+  {
+  }
 
+  #[Route('', name: 'index', methods: ['GET'])]
+  public function index(): Response
+  {
+    $products = $this->productService->getAllProducts();
+
+    return $this->render('catalog/product/index.html.twig', [
+      'products' => $products,
+    ]);
+  }
+
+  #[Route('/{id}', name: 'show', methods: ['GET'])]
+  public function show(int $id): Response
+  {
+    $product = $this->productService->getProduct($id);
+
+    if (!$product) {
+      throw $this->createNotFoundException();
+    }
+
+    return $this->render('catalog/product/show.html.twig', [
+      'product' => $product,
+    ]);
+  }
 }
--- src/Catalog/Service/ProductService.php
+++ src/Catalog/Service/ProductService.php
@@ -2,7 +2,32 @@
 
 namespace App\Catalog\Service;
 
+use App\Catalog\Entity\Product;
+use App\Catalog\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
 class ProductService
 {
+  public function __construct(
+    private EntityManagerInterface $em,
+    private ProductRepository $productRepository,
+  )
+  {
+  }
 
+  public function createProduct(Product $product): void
+  {
+    $this->em->persist($product);
+    $this->em->flush();
+  }
+
+  public function getProduct(int $id): ?Product
+  {
+    return $this->productRepository->find($id);
+  }
+
+  public function getAllProducts(): array
+  {
+    return $this->productRepository->findAll();
+  }
 }
